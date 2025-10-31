66d3a4ca8dc89853352b93ddb256635d531081ca
Vanshika Agarwal <vanshikaagarwal814@gmail.com>
2025-10-31 16:12:50 +0530

    Import additional TypeScript functions and classes, and update routing in TypeScriptEssentials component

--- client/src/pages/Notes/TypeScriptEssentials/TypeScriptEssentials.jsx
+++ client/src/pages/Notes/TypeScriptEssentials/TypeScriptEssentials.jsx
@@ -31,13 +31,13 @@ import Generics from './TsTopics/AdvancedTypes/Generics';
 import UtilityTypes from './TsTopics/AdvancedTypes/UtilityTypes';
 
 // Functions and Classes
-// import FunctionTypes from './TsTopics/FunctionsClasses/FunctionTypes';
-// import OptionalParameters from './TsTopics/FunctionsClasses/OptionalParameters';
-// import DefaultParameters from './TsTopics/FunctionsClasses/DefaultParameters';
-// import RestParameters from './TsTopics/FunctionsClasses/RestParameters';
-// import ClassTypes from './TsTopics/FunctionsClasses/ClassTypes';
-// import Interfaces from './TsTopics/FunctionsClasses/Interfaces';
-// import AbstractClasses from './TsTopics/FunctionsClasses/AbstractClasses';
+import FunctionTypes from './TsTopics/FunctionsClasses/FunctionTypes';
+import OptionalParameters from './TsTopics/FunctionsClasses/OptionalParameters';
+import DefaultParameters from './TsTopics/FunctionsClasses/DefaultParameters';
+import RestParameters from './TsTopics/FunctionsClasses/RestParameters';
+import ClassTypes from './TsTopics/FunctionsClasses/ClassTypes';
+import Interfaces from './TsTopics/FunctionsClasses/Interfaces';
+import AbstractClasses from './TsTopics/FunctionsClasses/AbstractClasses';
 
 // Modules and Namespaces
 // import Modules from './TsTopics/ModulesNamespaces/Modules';
@@ -126,14 +126,14 @@ const TypeScriptEssentials = () => {
                 <Route path="generics" element={<Generics />} />
                 <Route path="utility-types" element={<UtilityTypes />} />
 
-                {/* Functions and Classes - Will be added in PR #5 */}
-                {/* <Route path="function-types" element={<FunctionTypes />} />
+                {/* Functions and Classes */}
+                <Route path="function-types" element={<FunctionTypes />} />
                 <Route path="optional-parameters" element={<OptionalParameters />} />
                 <Route path="default-parameters" element={<DefaultParameters />} />
                 <Route path="rest-parameters" element={<RestParameters />} />
                 <Route path="class-types" element={<ClassTypes />} />
                 <Route path="interfaces" element={<Interfaces />} />
-                <Route path="abstract-classes" element={<AbstractClasses />} /> */}
+                <Route path="abstract-classes" element={<AbstractClasses />} />
 
                 {/* Modules and Namespaces - Will be added in PR #6 */}
                 {/* <Route path="modules" element={<Modules />} />
